57dffed1bbb84e556aaebea35d966caa8383344d
zhengh <zhengh@fenqi.im>
2019-11-25 19:42:43 +0800

    添加springboot项目

---
+++ startspring/src/test/java/com/zhxiansheng/startspring/StartspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhxiansheng.startspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
